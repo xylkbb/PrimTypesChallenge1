600d0e8053460da6b7e8dd29b01ea501ba7bc11b
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-05 13:35:03 -0400

    updated.

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Assignment {
+public class Assignment {
+    public static void main(String[] args) {
+
+
+
+        byte myByte = 10;
+        short myShort = 5;
+        int myInt = 3;
+
+        int sumOfEverything = myByte + myShort + myInt;
+
+
+        long myLong = 80000 - 5 * sumOfEverything;
+
+        System.out.println(myLong);
+
+
+    }
+
 }
